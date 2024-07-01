677c7b337b76529e8ee93bdf12efe28d495a50df
rohit verma <rohithverma.dev@gmail.com>
2024-07-01 14:17:43 +0530

    logic completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [random, setRandom ] = useState(true)
+  const [smooth, setSmooth] = useState(false)
+
+  useEffect(() => {
+    let interval_ID1;
+    let interval_ID2;
+    if (random) {
+       interval_ID1 =  setInterval(() => {
+          let hue = Math.random()*360;
+          let saturation = Math.random()*100;
+          let lightness = Math.random()*100;
+          document.body.style.backgroundColor =  `hsl(${hue}, ${saturation}%, ${lightness}%)`  
+      }, 1000);
+    }
+
+    if (smooth) {
+       let hue = 0
+       interval_ID2 =  setInterval(() => { 
+           hue=  hue<360 ? hue+1: 0
+          document.body.style.backgroundColor =  `hsl(${hue}, ${50}%, ${70}%)`
+      }, 20);
+    }
+
+    return () => {
+      clearInterval(interval_ID1);
+      clearInterval(interval_ID2);
+    };
+  }, [random , smooth])
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="buttons">
+        <button onClick={()=>{setRandom(true);setSmooth(false)}} className={random?"active": "" } >Random Color Generation</button>
+        <button onClick={()=>{setSmooth(true);setRandom(false);}} className={smooth?"active": "" } >Smooth Color Generation</button>
+      </div>
+    </>
   );
 }
 
